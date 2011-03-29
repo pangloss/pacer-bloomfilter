b9ee5af14c8946f46198f633b8a6f18f4198ed00
Darrick Wiebe <darrick@innatesoftware.com>
2011-03-29 01:54:51 -0400

    Put the pipes into a BloomFilter namespace. Also allow a prebuilt filter to be passed in.

--- lib/pacer/filter/bloomfilter.rb
+++ lib/pacer/filter/bloomfilter.rb
@@ -1,10 +1,11 @@
 module Pacer
   module Core
     module Route
-      def bloomfilter(false_pos_prob, expected_count, &block)
+      def bloomfilter(false_pos_prob, expected_count, opts = {}, &block)
         chain_route :filter => :bloom,
           :false_pos_prob => false_pos_prob,
           :expected_count => expected_count,
+          :filter => opts[:filter],
           :block => block
       end
     end
@@ -12,7 +13,7 @@ module Pacer
 
   module Filter
     module BloomFilter
-      attr_accessor :false_pos_prob, :expected_count, :block
+      attr_accessor :false_pos_prob, :expected_count, :block, :filter
 
       def uniq
         @except ||= []
@@ -32,10 +33,6 @@ module Pacer
         self
       end
 
-      def filter
-        @filter = true
-      end
-
     protected
 
       def attach_pipe(pipe)
@@ -45,18 +42,18 @@ module Pacer
       end
 
       def except_pipe(pipe)
-        bfp = Pacer::Pipes::BloomFilterReject.new false_pos_prob, expected_count, sideline_pipe
+        bfp = Pacer::Pipes::BloomFilter::RejectPipe.new false_pos_prob, expected_count, sideline_pipe
         bfp.accumulate if @uniq
         prepare_pipe(bfp, @except, pipe)
       end
 
       def only_pipe(pipe)
-        bfp = Pacer::Pipes::BloomFilterReject.new false_pos_prob, expected_count, sideline_pipe
-        bfp.accumulate if @uniq
+        bfp = Pacer::Pipes::BloomFilter::SelectPipe.new false_pos_prob, expected_count, sideline_pipe
         prepare_pipe(bfp, @except, pipe)
       end
 
       def prepare_pipe(bfp, all_items, pipe)
+        bfp.filter = filter if filter
         all_items.each do |items|
           if items.is_a? Enumerable
             bfp.addAll items
@@ -73,7 +70,6 @@ module Pacer
           Pacer::Route.pipeline Pacer::Route.empty(self).map(&block)
         end
       end
-
     end
   end
 end
--- lib/pacer/pipe/bloomfilter_reject.rb
+++ lib/pacer/pipe/bloomfilter_reject.rb
@@ -1,98 +1,103 @@
 module Pacer
   module Pipes
-    class SideliningPipe < AbstractPipe
-      def initialize(pipe)
-        super()
-        @sideline = pipe
-        @sidelineExpando = ExpandableIterator.new(java.util.ArrayList.new.iterator);
-        @sideline.setStarts(@sidelineExpando);
-      end
-
-    protected
-
-      def sidelineValue(value)
-        if @sideline
-          @sideline.reset
-          @sidelineExpando.add value
-          @sideline.next
-        else
-          value
-        end
-      rescue NativeException => e
-        if e.cause.getClass == Pacer::NoSuchElementException.getClass
-          nil
-        else
-          raise e
-        end
-      end
-    end
-
-    class BloomFilterReject < SideliningPipe
-      import com.skjegstad.utils.BloomFilter
-      field_accessor :starts
-
-      def initialize(false_pos_prob, expected_count, sideline_pipe = nil)
-        super(sideline_pipe)
-        @filter = BloomFilter.new(false_pos_prob, expected_count)
-      end
-
-      def addAll(elements)
-        @filter.addAll(elements)
-      end
-
-      def accumulate
-        @accumulate = true
-      end
-
-    protected
-
-      def processNextStart()
-        while raw_element = starts.next
-          value = sidelineValue(raw_element)
-          unless @filter.contains? value.to_s
-            @filter.add(value) if @accumulate
-            return raw_element
+    module BloomFilter
+      class SideliningPipe < AbstractPipe
+        def initialize(pipe)
+          super()
+          if pipe
+            @sideline = pipe
+            @sidelineExpando = ExpandableIterator.new(java.util.ArrayList.new.iterator);
+            @sideline.setStarts(@sidelineExpando);
           end
         end
-      rescue NativeException => e
-        if e.cause.getClass == Pacer::NoSuchElementException.getClass
-          raise e.cause
-        else
-          raise e
-        end
-      end
-    end
 
-    class BloomFilterSelect < SideliningPipe
-      import com.skjegstad.utils.BloomFilter
-      field_accessor :starts
+      protected
 
-      def initialize(false_pos_prob, expected_count, sideline_pipe = nil)
-        super(sideline_pipe)
-        @filter = BloomFilter.new(false_pos_prob, expected_count)
-      end
-
-      def addAll(elements)
-        @filter.addAll(elements)
-      end
-
-    protected
-
-      def processNextStart()
-        while raw_element = starts.next
-          value = sidelineValue(raw_element)
-          if @filter.contains? value.to_s
-            return raw_element
+        def sidelineValue(value)
+          if @sideline
+            @sideline.reset
+            @sidelineExpando.add value
+            @sideline.next
+          else
+            value
+          end
+        rescue NativeException => e
+          if e.cause.getClass == Pacer::NoSuchElementException.getClass
+            nil
+          else
+            raise e
           end
         end
-      rescue NativeException => e
-        if e.cause.getClass == Pacer::NoSuchElementException.getClass
-          raise e.cause
-        else
-          raise e
+      end
+
+      class RejectPipe < SideliningPipe
+        import com.skjegstad.utils.BloomFilter
+        field_accessor :starts
+        attr_accessor :filter
+
+        def initialize(false_pos_prob, expected_count, sideline_pipe = nil)
+          super(sideline_pipe)
+          @filter = BloomFilter.new(false_pos_prob, expected_count)
+        end
+
+        def addAll(elements)
+          @filter.addAll(elements)
+        end
+
+        def accumulate
+          @accumulate = true
+        end
+
+      protected
+
+        def processNextStart()
+          while raw_element = starts.next
+            value = sidelineValue(raw_element)
+            unless @filter.contains? value.to_s
+              @filter.add(value) if @accumulate
+              return raw_element
+            end
+          end
+        rescue NativeException => e
+          if e.cause.getClass == Pacer::NoSuchElementException.getClass
+            raise e.cause
+          else
+            raise e
+          end
+        end
+      end
+
+      class SelectPipe < SideliningPipe
+        import com.skjegstad.utils.BloomFilter
+        field_accessor :starts
+        attr_accessor :filter
+
+        def initialize(false_pos_prob, expected_count, sideline_pipe = nil)
+          super(sideline_pipe)
+          @filter = BloomFilter.new(false_pos_prob, expected_count)
+        end
+
+        def addAll(elements)
+          @filter.addAll(elements)
+        end
+
+      protected
+
+        def processNextStart()
+          while raw_element = starts.next
+            value = sidelineValue(raw_element)
+            if @filter.contains? value.to_s
+              return raw_element
+            end
+          end
+        rescue NativeException => e
+          if e.cause.getClass == Pacer::NoSuchElementException.getClass
+            raise e.cause
+          else
+            raise e
+          end
         end
       end
     end
-
   end
 end

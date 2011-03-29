a63089aaa7f00981708195b640025f06fda62d7f
Darrick Wiebe <darrick@innatesoftware.com>
2011-03-29 01:53:25 -0400

    Put in the standard gem boilerplate stuff.

--- lib/pacer-bloomfilter.rb
+++ lib/pacer-bloomfilter.rb
@@ -1,4 +1,21 @@
-$: << File.dirname(__FILE__)
-require File.dirname(__FILE__) + '/../vendor/java-bloomfilter.jar'
+module PacerBloomFilter
+  unless const_defined? :VERSION
+    PATH = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+    VERSION = File.read(PATH + '/VERSION').chomp
+
+    $: << File.dirname(__FILE__)
+    require File.dirname(__FILE__) + '/../vendor/java-bloomfilter.jar'
+  end
+
+  def self.reload!
+    Dir[File.dirname(__FILE__) + '/**/*.rb'].each do |file|
+      puts file
+      load file
+    end
+    nil
+  end
+end
+
 require 'pacer/pipe/bloomfilter_reject'
 require 'pacer/filter/bloomfilter'
+
